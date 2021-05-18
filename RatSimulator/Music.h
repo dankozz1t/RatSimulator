d0375f6304d0c1116419e70ebdeb342aaffe1d2a
Alex <AlexDankoXXL@gmail.com>
2021-05-18 23:44:56 +0300

    Добавлена музыка

--- RatSimulator/Music.h
+++ RatSimulator/Music.h
@@ -1,8 +1,6 @@
 ﻿#pragma once
-
 #pragma comment(lib, "winmm.lib")
 #include <mmsystem.h>
-
 #include<stdlib.h>
 #include <filesystem>
 #include"Func.h"
@@ -10,7 +8,6 @@
 using namespace std;
 using std::filesystem::current_path;
 const wstring dir = current_path().wstring();
-
 
 void mciSend(wstring ws) // Сокращение //mciSendStringW(L"open \"Resources/DeloDone.mp3\" type mpegvideo alias music", NULL, 0, 0);
 {
@@ -38,14 +35,6 @@ void playCoins() {
 	PlaySound(_dir.c_str(), NULL, SND_ASYNC | SND_FILENAME);
 }
 
-
-void playRozbiinik() {
-
-	wstring _dir = dir;
-	_dir.append(L"\\Resources\\Rozbiinik.wav");
-	PlaySound(_dir.c_str(), NULL, SND_ASYNC | SND_FILENAME);
-}
-
 void playLetMeDay() {
 
 	wstring _dir = dir;
@@ -54,10 +43,10 @@ void playLetMeDay() {
 }
 
 
-void playNope() {
+void playError() {
 
 	wstring _dir = dir;
-	_dir.append(L"\\Resources\\nope.wav");
+	_dir.append(L"\\Resources\\error.wav");
 	PlaySound(_dir.c_str(), NULL, SND_ASYNC | SND_FILENAME);
 }
 
@@ -101,5 +90,55 @@ void playPositiveSound() {
 
 	wstring _dir = dir;
 	_dir.append(L"\\Resources\\positive.wav");
+	PlaySound(_dir.c_str(), NULL, SND_ASYNC | SND_FILENAME);
+}
+
+void playWelcomeShop() {
+
+	wstring _dir = dir;
+	_dir.append(L"\\Resources\\Welcome.wav");
+	PlaySound(_dir.c_str(), NULL, SND_ASYNC | SND_FILENAME);
+}
+
+void playGameOver() {
+
+	wstring _dir = dir;
+	_dir.append(L"\\Resources\\gameOver.wav");
+	PlaySound(_dir.c_str(), NULL, SND_ASYNC | SND_FILENAME);
+}
+
+void playWin() {
+
+	wstring _dir = dir;
+	_dir.append(L"\\Resources\\Win.wav");
+	PlaySound(_dir.c_str(), NULL, SND_ASYNC | SND_FILENAME);
+}
+
+void playImHungry() {
+
+	wstring _dir = dir;
+	_dir.append(L"\\Resources\\ImHungry.wav");
+	PlaySound(_dir.c_str(), NULL, SND_ASYNC | SND_FILENAME);
+}
+
+void playLeaf() {
+
+	wstring _dir = dir;
+	_dir.append(L"\\Resources\\leaf.wav");
+	PlaySound(_dir.c_str(), NULL, SND_ASYNC | SND_FILENAME);
+}
+
+void playPurchase() {
+
+	wstring _dir = dir;
+	_dir.append(L"\\Resources\\Purchase.wav");
+	PlaySound(_dir.c_str(), NULL, SND_ASYNC | SND_FILENAME);
+}
+
+
+void playGetOut() {
+
+	wstring _dir = dir;
+	_dir.append(L"\\Resources\\GetOut.wav");
 	PlaySound(_dir.c_str(), NULL, SND_ASYNC | SND_FILENAME);
 }
